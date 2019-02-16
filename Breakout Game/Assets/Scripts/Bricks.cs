a484f73560f5cea4e7fc17b9b60400df8a68394c
mathew1122 <mandork57@gmail.com>
2019-02-16 00:33:28 -0800

    Game and exe upload
    Uploaded the game and executable files.

---
+++ Breakout Game/Assets/Scripts/Bricks.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour 
+{
+
+	public GameObject brickParticle;
+
+	void OnCollisionEnter()
+	{
+		Instantiate (brickParticle, transform.position, Quaternion.identity);
+		Debug.Log ("destroy");
+		GM.instance.DestroyBrick ();
+		Destroy (gameObject);
+	}
+}
